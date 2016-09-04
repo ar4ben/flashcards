0d0fca3b7d0f55ae702a023c5df59c8b49c606cb
ar4ben <arthur.rodzkin@gmail.com>
2016-09-05 02:03:15 +0300

    add edit/create/delete card and some validations

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,20 @@
 class Card < ActiveRecord::Base
+  validates :original_text, presence: true
+  validates :translated_text, presence: true
+  validates :review_date, presence: true
+  validate :translated_should_be_not_equal_to_original
+
+  before_validation :set_review_date
+
+  protected
+
+    def translated_should_be_not_equal_to_original
+      if original_text.downcase == translated_text.downcase
+        errors.add(:translated_text, "перевод не может быть равен начальному тексту")
+      end
+    end
+
+    def set_review_date
+      self.review_date = DateTime.now + 3
+    end
 end
