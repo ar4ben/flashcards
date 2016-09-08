9c5164b391c9b18adb5087a3668be3513663e90d
ar4ben <arthur.rodzkin@gmail.com>
2016-09-09 01:14:06 +0300

    use seed.rb for creating cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# ruby encoding: utf-8
+require 'nokogiri'
+require 'open-uri'
+
+def create_words(page)
+	page.xpath('//tr[contains(@class,"row") and ./td[3]/text()[string-length() > 1]]').each do |word|
+		original = word.xpath('./td[@class="bigLetter"]').text
+		translated = word.xpath('./td[3]').text
+		puts "creating word #{original} - #{translated}"
+		Card.create(original_text: original, translated_text: translated)
+	end
+end
+
+def create_pages_with_words(page)
+	base_url = "http://www.languagedaily.com"
+	page.xpath('//h3[contains(text(),"Index of most common")]/following-sibling::ul//a[@href]/@href').each do |link|
+		puts "downloading page #{base_url}#{link.text}..."
+		new_page = Nokogiri::HTML(open("#{base_url}#{link.text}"))
+		create_words(new_page)
+	end
+end
+
+seed_url = Nokogiri::HTML(open("http://www.languagedaily.com/learn-german/vocabulary/common-german-words-11"))
+create_pages_with_words(seed_url)
+create_words(seed_url)
