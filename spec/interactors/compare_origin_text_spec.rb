4073566cbc8466b60a4e9bdc2e56368f01c73399
ar4ben <arthur.rodzkin@gmail.com>
2016-09-22 11:27:42 +0300

    add interactor test

---
+++ spec/interactors/compare_origin_text_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+describe CompareOriginText do
+  before do
+    @card = Card.create(original_text: 'x', translated_text: 'y')
+    @params = {card_id: @card.id}
+  end
+
+  it ".call should compare user text with original" do
+    @params[:original_text] = 'x'
+    interactor = CompareOriginText.call(params: @params)
+    expect(interactor).to be_a_success
+    @params[:original_text] = 'z'
+    interactor = CompareOriginText.call(params: @params)
+    expect(interactor).to_not be_a_success
+  end
+end
