47284e84177b193cb47083e27bfd209e7d5d939c
ar4ben <arthur.rodzkin@gmail.com>
2016-09-26 08:44:24 +0300

    add feature test

---
+++ spec/features/translation_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'Translation check' do
+  before(:each) do
+    create(:card)
+    visit root_path
+  end
+
+  context "with right original text" do
+    it "should be success" do
+      fill_in("random_card_original_text", with: "run")
+      click_button "Проверить"
+      expect(page).to have_content("Правильно!")
+    end
+  end
+
+  context "with wrong original text" do
+    it "should be failed" do
+      fill_in("random_card_original_text", with: "бежать")
+      click_button "Проверить"
+      expect(page).to have_content("Неправильно!")
+    end
+  end
+end
