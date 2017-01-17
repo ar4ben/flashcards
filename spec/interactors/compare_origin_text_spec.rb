9c9d09e1ba086bfbb9e8f62df9d446047793c748
ar4ben <arthur.rodzkin@gmail.com>
2017-01-17 05:33:52 +0300

    add test for levenshtein check

--- spec/interactors/compare_origin_text_spec.rb
+++ spec/interactors/compare_origin_text_spec.rb
@@ -8,8 +8,10 @@ describe CompareOriginText do
     @params = { card_id: @card.id }
   end
 
-  def set_correct_review_date(next_review, success)
-    @params[:original_text] = if success
+  def set_correct_review_date(next_review, success, levenshtein = false)
+    @params[:original_text] = if levenshtein
+                                "#{@card.original_text}!"
+                              elsif success
                                 @card.original_text
                               else
                                 @card.translated_text
@@ -19,6 +21,7 @@ describe CompareOriginText do
     card_review = @card.review_date.strftime("%d-%H")
     expected_review = next_review.since.strftime("%d-%H")
     expect(card_review).to eq(expected_review)
+    expect(@card.fail).to eq(0) if levenshtein
   end
 
   it ".call should compare user text with original" do
@@ -39,5 +42,6 @@ describe CompareOriginText do
     set_correct_review_date(1.month, false)
     set_correct_review_date(1.month, false)
     set_correct_review_date(12.hours, false)
+    set_correct_review_date(12.hours, true, true)
   end
 end
